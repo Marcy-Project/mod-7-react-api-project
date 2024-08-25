ce00b81fd379dfcb0f0e47a0647637b5ddb09103
LinqingZ <linqing201@gmail.com>
2024-08-24 22:24:44 -0400

    convert all code to react

---
+++ Pokemon2.0/src/components/GoHomeButton.jsx
@@ -0,0 +1,13 @@
+import { useNavigate } from 'react-router-dom';
+
+const GoHomeButton = () => {
+  const navigate = useNavigate();
+
+  return (
+    <button className="ui button fluid" onClick={() => navigate('/')}>
+      Go Home
+    </button>
+  );
+};
+
+export default GoHomeButton;
---
+++ Pokemon2.0/src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import SearchForm from '../components/SearchForm';
+import RandomPokemon from '../components/RandomPokemon';
+
+const HomePage = () => {
+
+  return (
+    <div>
+      <SearchForm />
+      <RandomPokemon />
+      
+    </div>
+  );
+};
+
+export default HomePage;
