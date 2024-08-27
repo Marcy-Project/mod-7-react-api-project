50cec910bf745921c4df7a44b5741beb804dc6f0
Mei Zhu <maggiez0613@gmail.com>
2024-08-26 23:35:52 -0400

    fixed ui of pokemonlist, card, need to fix icon buttons and rest

--- Pokemon2.0/src/App.jsx
+++ Pokemon2.0/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import { Route, Routes, Link } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import NotFoundPage from './pages/NotFoundPage';
